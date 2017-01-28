78fd09482fd7164b930d6db7cb0aee0607aae090
Arunkumar-MS <arunkumar.shivanna@in.tesco.com>
2017-01-28 14:21:50 +0530

    website

---
+++ css/sw.js
@@ -0,0 +1,45 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/css/sw.js').then(function(reg) {
+    console.log('◕‿◕', reg);
+  }, function(err) {
+    console.log('ಠ_ಠ', err);
+  });
+}
+
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        'images/v1.mp4'
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  var response;
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(r) {
+    response = r;
+    caches.open('v1').then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('/sw-test/gallery/myLittleVader.jpg');
+  }));
+});
+
+this.addEventListener('activate', function(event) {
+  var cacheWhitelist = ['arun-v1'];
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
